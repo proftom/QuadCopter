c8285188f9eb859a60fd9157e05736252d5a8fd0
Joshua Elsdon <je310@imperial.ac.uk>
2013-06-13 19:08:45 +0100

    neatened and working

--- PCL/Vision.h
+++ PCL/Vision.h
@@ -68,7 +68,7 @@ class QCVision
 	unsigned int eventflag; 
 	unsigned int Perseventflag; 
 	
-    pcl::visualization::CloudViewer viewer;
+   // pcl::visualization::CloudViewer viewer;
     std::string device_id_;
     boost::mutex mtx_;
 	boost::mutex m_mutexLockPlanes;
@@ -88,7 +88,7 @@ class QCVision
 	//PCL_Connector con;						//
 	
 
-	QCVision (const std::string& device_id = "") : viewer ("PCL OpenNI NormalEstimation Viewer"), device_id_(device_id), eventflag(1)
+	QCVision (const std::string& device_id = "") :  device_id_(device_id), eventflag(1)
     {
 
       ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::SIMPLE_3D_GRADIENT);
@@ -96,7 +96,7 @@ class QCVision
 	  ne_.setMaxDepthChangeFactor (G_depthdepend);
       ne_.setNormalSmoothingSize (G_smoothsize);
       new_cloud_ = false;
-      viewer.registerKeyboardCallback(&QCVision::keyboard_callback, *this);
+      //viewer.registerKeyboardCallback(&QCVision::keyboard_callback, *this);
 	  //Plane cloud
 	  planes_.assign(19200, Plane(0,0,0,0));
 
@@ -109,54 +109,24 @@ class QCVision
 
 	
 	void cloud_cb (const CloudConstPtr& cloud)
-    {
-	  //t2 = clock(); 
-	  boost::mutex::scoped_lock lock (mtx_);
+    {	  boost::mutex::scoped_lock lock (mtx_);
  
       //lock while we set our cloud;
       FPS_CALC ("computation");
-      // Estimate surface normals
-
-	  // Cloud cld (new Cloud(cloud));
 
       normals_.reset (new pcl::PointCloud<pcl::Normal>);
-	  //cld_render_ptr.reset(new pcl::PointCloud<PointType>);
-	  
-      //double start = pcl::getTime ();
+
       ne_.setInputCloud (cloud);
 	  ne_.compute (*normals_); 
 
-      //double stop = pcl::getTime ();
+
  
       cloud_ = cloud;
 
 	  static bool hasrun = false;
 	  if(eventflag & 0x1){
 		  if(!hasrun){
-				//hasrun = true;
-				
-				//ofstream myfile;
-				//myfile.open("planes.txt",ios::app);
-				//myfile << planes.size() << endl;
 
-				//for (int i = 0; i < planes.size(); i++) {
-				//	planes[i].calculateCovarianceMatrix(planes_);
-				//	double dist = sqrt( (planes[i].A * planes[i].A) + (planes[i].B * planes[i].B) + (planes[i].C * planes[i].C));
-
-				//	myfile << planes[i].A * (1.0 / dist) << " " << planes[i].B * (1.0 / dist) << " " << planes[i].C * (1.0 / dist) << " " << planes[i].D << endl;
-				//	for (int j = 0; j < planes[i].covariance.size(); j++) {
-				//		myfile << planes[i].covariance[j][0] << " " << planes[i].covariance[j][1] << " " << planes[i].covariance[j][2] << " " << planes[i].covariance[j][3] << endl;
-				//	}
-				//	myfile << planes[i].indicies.size() << endl;
-				//	
-				//}
-				//
-				//myfile << t2 << endl;
-				//myfile << "<<<" << endl; 
-				
-				//vector<Plane> h = DBScanND(planes,1,0.05);
-
-				//For displaying coloured planes
 
 				pcl::PointCloud<PointType> pc(*cloud);
 				correctDistances(cloud, &pc);
@@ -165,26 +135,13 @@ class QCVision
 				cloud_ = correctedCloud;				
 				
 				//Now flood fill 
-				vector<vector<int>> clusterIndicies = floodFillAll(1000, 20);//DBSCAN(G_epsilon, G_minpts);
-				//Found planes
+				vector<vector<int>> clusterIndicies = floodFillAll(1000, 20);
+
 				planes = ClusterToAveragePlane(clusterIndicies);
-
-				//ofstream myfile;
-				//myfile.open("planes.txt",ios::app);
-				//myfile << planes.size() << endl;
-
-				//Lock anything from trying to read the planes
-				
 
 				for (int i = 0; i < planes.size(); i++) {
 					planes[i].calculateCovarianceMatrix(planes_);
-					//float dist = sqrt( (planes[i].A * planes[i].A) + (planes[i].B * planes[i].B) + (planes[i].C * planes[i].C));
-
-					//myfile << planes[i].A * (1.0 / dist) << " " << planes[i].B * (1.0 / dist) << " " << planes[i].C * (1.0 / dist) << " " << planes[i].D << endl;
-					//for (int j = 0; j < planes[i].covariance.size(); j++) {
-					//	myfile << planes[i].covariance[j][0] << " " << planes[i].covariance[j][1] << " " << planes[i].covariance[j][2] << " " << planes[i].covariance[j][3] << endl;
-					//}
-					//myfile << planes[i].indicies.size() << endl;
+			
 					
 				}
 
@@ -195,29 +152,7 @@ class QCVision
 				bNewSetOfPlanes = true;
 				//cout << "Unlocked planes in Vision class";
 				m_mutexLockPlanes.unlock();
-				
-				//myfile << t2 << endl;
-				//myfile << "<<<" << endl; 
-
-				
-
-				////Colour clusters
-				//for(int i = 0; i < clusterIndicies.size(); i++) 
-				//{
-				//	char r,b,g;
-				//	r = rand()%256;
-				//	g = rand()%256;
-				//	b = rand()%256;
-				//	for(int j = 0; j < clusterIndicies[i].size(); j++) 
-				//	{
-				//		pc.points[clusterIndicies[i][j]].r = r;
-				//		pc.points[clusterIndicies[i][j]].g = g;
-				//		pc.points[clusterIndicies[i][j]].b = b;
-				//	}
-				//}
-
-				//CloudConstPtr inputtmp(new pcl::PointCloud<PointType>(pc));
-				//cloud_ = inputtmp;
+			
 
 				new_cloud_ = true;
 		  }
@@ -244,93 +179,8 @@ class QCVision
 		}
     }
 
-    void viz_cb (pcl::visualization::PCLVisualizer& viz)
-    {
-      mtx_.lock ();
-      if (!cloud_ || !normals_)
-      {
-        //boost::this_thread::sleep(boost::posix_time::seconds(1));
-        mtx_.unlock ();
-        return;
-      }
-
-      CloudConstPtr temp_cloud;
-      pcl::PointCloud<pcl::Normal>::Ptr temp_normals;
-
-      temp_cloud.swap (cloud_); //here we set cloud_ to null, so that
-      temp_normals.swap (normals_);
-      mtx_.unlock ();
-
-      if (!viz.updatePointCloud (temp_cloud, "OpenNICloud"))
-      {
-        viz.addPointCloud (temp_cloud, "OpenNICloud");
-		viz.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "OpenNICloud");
-        viz.resetCameraViewpoint ("OpenNICloud");
-      }
-      // Render the data
-      if (new_cloud_)
-      {
-
-        viz.removePointCloud ("normalcloud");
-
-        //viz.addPointCloudNormals<PointType, pcl::Normal> (temp_cloud, temp_normals, 5, 0.05f, "normalcloud");
-        new_cloud_ = false;
-
-      }
-    }
-
-    void keyboard_callback (const pcl::visualization::KeyboardEvent& event, void*)
-    {
-		ofstream MyFile;
-		MyFile.open ("data1.csv", ios::out | ios::ate | ios::app) ;
-      boost::mutex::scoped_lock lock (mtx_);
-      
-	  switch (event.getKeyCode ())
-      {
-        case '1':
-
-       /*   ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::COVARIANCE_MATRIX);
-          std::cout << "switched to COVARIANCE_MATRIX method\n";
-			(cloud_dbscanproc->width, cloud_dbscanproc->height, cloud_dbscanproc);*/
-			for (int p = 0; p <19200; p++){
-
-				MyFile << cloud_->points[p].x << "," << cloud_->points[p].y <<","<< cloud_->points[p].z<<"\n";
-
-			}
 
 
-			MyFile << "//\n"; 
-          break;
-        case '2':
-
-		  
-          ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::AVERAGE_3D_GRADIENT);
-          std::cout << "switched to AVERAGE_3D_GRADIENT method\n";
-          break;
-        case '3':
-          ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::AVERAGE_DEPTH_CHANGE);
-          std::cout << "switched to AVERAGE_DEPTH_CHANGE method\n";
-          break;
-        case '4':
-          ne_.setNormalEstimationMethod (pcl::IntegralImageNormalEstimation<PointType, pcl::Normal>::SIMPLE_3D_GRADIENT);
-          std::cout << "switched to SIMPLE_3D_GRADIENT method\n";
-          break;
-		case '5':
-			ne_.setMaxDepthChangeFactor (G_depthdepend);
-			ne_.setNormalSmoothingSize (G_smoothsize);
-			eventflag |= 0x1;
-			break;
-		case '6':
-			eventflag &= ~0x1;
-			break;
-		case '7':
-			Perseventflag |= 0x1;
-			break;
-		case '8':
-			Perseventflag &= ~0x1;
-			break;
-      }
-    }
 
     void run ()
     {
@@ -339,11 +189,11 @@ class QCVision
       boost::function<void (const CloudConstPtr&)> f = boost::bind (&QCVision::cloud_cb, this, _1);
       boost::signals2::connection c = interface->registerCallback (f);
 
-      viewer.runOnVisualizationThread (boost::bind(&QCVision::viz_cb, this, _1), "viz_cb");
+      //viewer.runOnVisualizationThread (boost::bind(&QCVision::viz_cb, this, _1), "viz_cb");
 
       interface->start ();
 
-      while (!viewer.wasStopped ())
+      while (1)
       {
         boost::this_thread::sleep(boost::posix_time::seconds(1));
       }
@@ -379,118 +229,7 @@ class QCVision
 		return planes;
 	}
 
-	vector<Plane> DBScanND(vector<Plane> planes, int minPoints, float epsilon) 
-	{
-		int sizeOfData = planes.size();
-		vector<bool> Visited(sizeOfData, false);
-		vector<bool> addedToCluster(sizeOfData, false);
-
-		//Clusters
-		vector<vector<int>> clusters;
-		
-		int clusterInd = 0;
-
-		for(int i = 0; i<sizeOfData; i++){
-			if(Visited[i]==false){
-				Visited[i] = true;
-			
-				vector<int> NeighborPts = regionQuery(planes, i, epsilon);
-				vector<bool> pointsInNeighborPts(sizeOfData, false);
-
-				if(!(NeighborPts.size()<minPoints))
-				{
-					vector<int> currentCluster;
-					expandCluster(i, NeighborPts, pointsInNeighborPts, currentCluster, epsilon, minPoints, Visited, addedToCluster, planes);
-					clusters.push_back(currentCluster);
-					clusterInd++;
-				}
-			}
-
-		}
-
-		cout << "Planes merging from " << planes.size() << " to " << clusters.size() << endl;
-		
-
-		//Now create a structure with the planes averaged according to how many elements are within the plane
-		//Make each element unique
-		std::vector<Plane> mergedPlanes;
-		for (int i = 0; i < clusters.size(); i++) {
-
-			//O(n log n)
-			std::set<int> unique (clusters[i].begin(), clusters[i].end());
-			Plane mergingPlane(0,0,0,0);
-			int nTotalIndicies = 0;
-			for (set<int>::iterator it=unique.begin() ; it != unique.end(); it++ ) {
-
-				mergingPlane.A += (planes[(*it)].A * planes[(*it)].indicies.size());
-				mergingPlane.B += (planes[(*it)].B * planes[(*it)].indicies.size());
-				mergingPlane.C += (planes[(*it)].C * planes[(*it)].indicies.size());
-				mergingPlane.D += (planes[(*it)].D * planes[(*it)].indicies.size());
-				mergingPlane.indicies.insert(mergingPlane.indicies.end(), planes[(*it)].indicies.begin(), planes[(*it)].indicies.end());
-				nTotalIndicies += planes[(*it)].indicies.size();
-			
-			}
-			mergingPlane.A /= nTotalIndicies; mergingPlane.B /= nTotalIndicies; mergingPlane.C /= nTotalIndicies; mergingPlane.D /= nTotalIndicies;
-			mergingPlane.calculateCovarianceMatrix(planes_);
-			mergedPlanes.push_back(mergingPlane);
-
-		}
-
-		//mergeMelo(mergedPlanes[0], mergedPlanes[1]);
-
-		//myfile << mergedPlanes.size() << endl;
-		
-
-
-		return mergedPlanes;
-	}
-
-	//Get all planes in region within epsilon
-	vector<int> regionQuery(vector<Plane> &planes, int currentPoint, float epsilon) {
-		vector<int> k_indicies;
-
-		for( int j = 0; j < planes.size(); j++) {
-			if	(	(abs(planes[currentPoint].A - planes[j].A) < epsilon) 
-				&&	(abs(planes[currentPoint].B - planes[j].B) < epsilon) 
-				&&	(abs(planes[currentPoint].C - planes[j].C) < epsilon)
-				&&	(abs(planes[currentPoint].D - planes[j].D) < epsilon)				)
-			{
-				k_indicies.push_back(j);
-			}
-		}
-
-		return k_indicies;
-	}
-
-	void expandCluster(	int inputInd, vector<int> NeighborPts, vector<bool> pointsInNeighborPts, vector<int> &currentCluster,
-					float radius, int minPoints, vector<bool> &Visited, vector<bool> &addedToCluster, vector<Plane> &planes){
-
-		currentCluster.push_back(inputInd);
-
-		for(int j = 0; j < NeighborPts.size(); j++)
-		{
-			if(Visited[NeighborPts[j]]==false){
-				Visited[NeighborPts[j]]=true;
-				vector<int> secondNeighborPts = regionQuery(planes, NeighborPts[j],radius);
-				if(secondNeighborPts.size()>=minPoints){
-					conditionalInsert(NeighborPts, pointsInNeighborPts, secondNeighborPts);
-				}
-			}
-			if (!addedToCluster[j])
-				currentCluster.push_back(NeighborPts[j]);
-				//if neighborPts[j] has yet to be added to a cluster add it to this one
-		}
-
-	}
-
-	void conditionalInsert(std::vector<int>& destination, std::vector<bool>& isInDest, std::vector<int> source){
-		for (int i = 0; i < source.size(); i++) {
-			if (!isInDest[source[i]]) {
-				destination.push_back(source[i]);
-				isInDest[source[i]] = true;
-			}
-		}
-	}
+	
 	
 	//Flood fill
 	vector<vector<int>> floodFillAll(int minPts, int maxTries) 
